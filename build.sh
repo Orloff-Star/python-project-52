4a7b509957a4aeb0bd3f098090a7c2ab5b0939d7
Eagle <ii-sys@mail.ru>
2025-02-06 14:38:41 +0700

    first commit

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
 
 # Установка зависимостей, сборка статики, применение миграций
-
+uv pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
