c53e1190973372a46b4c4ec2b228c19eefa3aef0
Eagle <ii-sys@mail.ru>
2025-02-11 13:14:15 +0700

    second commit

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.urls import path
 from task_manager import views
-from . import views
+
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='home'),
--- task_manager/views.py
+++ task_manager/views.py
@@ -2,6 +2,19 @@ from django.views.generic import TemplateView
 from django.http import HttpResponse
 from django.shortcuts import render
 
+import os
+
+from django.conf import settings
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+    def index_view(request):
+        print("Текущая рабочая директория:", os.getcwd())
+        print("Директории шаблонов:", settings.TEMPLATES[0]['DIRS'])
+        return render(request, 'index.html')
+
+def index_view(request):
+        print("Текущая рабочая директория:", os.getcwd())
+        print("Директории шаблонов:", settings.TEMPLATES[0]['DIRS'])
+        return render(request, 'index.html')
