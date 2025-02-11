3089f48db22d491b83df6c99e06f38379ae4dc74
Eagle <ii-sys@mail.ru>
2025-02-11 11:33:43 +0700

    second commit

--- task_manager/views.py
+++ task_manager/views.py
@@ -2,8 +2,6 @@ from django.views.generic import TemplateView
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def home(request):
-    return HttpResponse("Приветствие!")
 
 class IndexView(TemplateView):
     template_name = 'index.html'
