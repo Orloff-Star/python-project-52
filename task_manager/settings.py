77dd706e82c3ce4102422aad6c229ef18f03fe09
Eagle <ii-sys@mail.ru>
2025-02-12 10:33:55 +0700

    third commit 2

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -33,7 +33,10 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['python-project-52-r60m.onrender.com']
+ALLOWED_HOSTS = [
+    'python-project-52-r60m.onrender.com',
+    '127.0.0.1', 'localhost'
+]
 
 
 # Application definition
--- task_manager/urls.py
+++ task_manager/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
+from django.urls import include, path
 from task_manager import views
 
 
 urlpatterns = [
     path('', views.IndexView.as_view()),
+    path('users/', include('task_manager.users.urls')),
 ]
