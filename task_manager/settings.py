589fa4e85bfcdc908d88579d23bd8e8e55adf243
Eagle <ii-sys@mail.ru>
2025-03-05 10:36:11 +0700

    udate sittings

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -94,28 +94,22 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 
 '''DATABASES = {
     'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
-}'''
+}
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
-
+}'''
 
 DATABASE_URL = os.getenv('DATABASE_URL')
 
-if DATABASE_URL:
-    DATABASES = {
-        'default':
-            dj_database_url.config(
-                default=DATABASE_URL,
-                conn_max_age=600,
-                conn_health_checks=True
-            ),
-    }
-
-
+DATABASES = {
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600,
+    )
+}
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
