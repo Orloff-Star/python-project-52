c233a51b14fd6196d03a11e84fb8e904b626cdbc
Eagle <ii-sys@mail.ru>
2025-03-13 15:43:48 +0700

    update forms tasks

--- task_manager/tasks/forms.py
+++ task_manager/tasks/forms.py
@@ -3,11 +3,18 @@ from .models import Task
 from task_manager.labels.models import Label
 from task_manager.statuses.models import Status
 from django.utils.translation import gettext as _
+from django.contrib.auth.models import User
 
 
 class TaskForm(forms.ModelForm):
     name = forms.CharField(label=_('Name'))
     status = forms.ModelChoiceField(queryset=Status.objects.all(), label=_("Status"))
+    executor = forms.ModelChoiceField(
+        queryset=User.objects.all(),
+        required=False,
+        label=_('Executor'),
+        widget=forms.Select(attrs={'class': 'form-select'})  # Добавьте стили, если нужно
+    )
     labels = forms.ModelMultipleChoiceField(
         queryset=Label.objects.all(),
         widget=forms.SelectMultiple(attrs={'class': 'form-select'}),
@@ -18,8 +25,4 @@ class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ['name', 'description', 'status', 'executor', 'labels']
-        labels = {
-            'description': _('Description'),
-            'executor': _('Executor'),
-#            'labels': _('Labels'),
-        }
+        labels = {'description': _('Description')}
