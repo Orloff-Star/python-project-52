e1278c88b705eb2ebc6caf3505ca7777e3828690
Eagle <ii-sys@mail.ru>
2025-03-13 10:48:54 +0700

    updete User_update password

--- task_manager/form.py
+++ task_manager/form.py
@@ -6,5 +6,5 @@ from django.contrib.auth.forms import AuthenticationForm
 class CustomAuthenticationForm(AuthenticationForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['username'].label = _('Username')
+        self.fields['username'].label = _("Имя пользователя")
         self.fields['password'].label = _("Password")
