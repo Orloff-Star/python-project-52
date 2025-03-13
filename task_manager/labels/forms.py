4c69c6ab613843029f90c2c9b560136a15d4f715
Eagle <ii-sys@mail.ru>
2025-03-13 14:41:59 +0700

    update forms labels

--- task_manager/labels/forms.py
+++ task_manager/labels/forms.py
@@ -1,8 +1,11 @@
 from django import forms
 from .models import Label
+from django.utils.translation import gettext as _
 
 
 class LabelForm(forms.ModelForm):
+    name = forms.CharField(label=_('Name'), widget=forms.TextInput())
+    
     class Meta:
         model = Label
         fields = ['name']
