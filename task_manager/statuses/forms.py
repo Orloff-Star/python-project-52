cb13896fb605956ef36a6a4dca9571a5fbdde6c8
Eagle <ii-sys@mail.ru>
2025-03-13 14:05:59 +0700

    update forms statuses

--- task_manager/statuses/forms.py
+++ task_manager/statuses/forms.py
@@ -1,8 +1,11 @@
 from django import forms
 from .models import Status
+from django.utils.translation import gettext as _
 
 
 class StatusForm(forms.ModelForm):
+    name = forms.CharField(label=_('Name'), widget=forms.TextInput())
+
     class Meta:
         model = Status
         fields = ['name']
