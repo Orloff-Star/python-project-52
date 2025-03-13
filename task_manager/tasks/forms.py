b4a6cdb89cc0203f9fa73f69e6885d5ad8f25f55
Eagle <ii-sys@mail.ru>
2025-03-13 15:00:03 +0700

    update forms tasks

--- task_manager/tasks/forms.py
+++ task_manager/tasks/forms.py
@@ -1,9 +1,13 @@
 from django import forms
 from .models import Task
 from task_manager.labels.models import Label
+from task_manager.statuses.models import Status
+from django.utils.translation import gettext as _
 
 
 class TaskForm(forms.ModelForm):
+    name = forms.CharField(label=_('Name'))
+    status = forms.ModelChoiceField(queryset=Status.objects.all(), label=_("Status"))
     labels = forms.ModelMultipleChoiceField(
         queryset=Label.objects.all(),
         widget=forms.SelectMultiple(attrs={'class': 'form-select'}),
